309b5b281fdbb70891340d67d770d3aeea4cc471
dStass <dstiasny@outlook.com>
2020-10-31 22:06:00 +1100

    recovered files after laptop crash

--- backend/query.py
+++ backend/query.py
@@ -1,18 +1,9 @@
 import json
+from json import dump
 import requests
 import sys
 
 import boto3
-
-def read_json_from_file(filepath):
-    with open(filepath) as f:
-        return json.loads(f.read())
-
-def authorise_endpoint(endpoint, api_key):
-    return endpoint + '?apiKey=' + api_key
-
-def add_ingredients_to_endpoint(endpoint, ingredient_list):
-    return endpoint + '&ingredients=' + ',+'.join(ingredient_list)
 
 def get_aws_client(resource_name, aws_information):
     return boto3.client(
@@ -22,38 +13,120 @@ def get_aws_client(resource_name, aws_information):
         region_name = aws_information['region_name']
     )
 
-def get_recipes_query(api_key, user_input, aws_information):
+def read_json_from_file(filepath):
+    with open(filepath) as f:
+        return json.loads(f.read())
+
+def dump_json_to_filepath(dict_to_dump, filepath):
+    with open(filepath, 'w') as f:
+        f.write(json.dumps(dict_to_dump))
+
+
+# # # # # # # # # # # #
+#   ENDPOINT BUILDER  #
+# # # # # # # # # # # #
+
+def build_endpoint(api_key, query_info, original_endpoint):
     '''
-    user_input = json file from frontend
+    Use the decorator pattern to add information to the endpoint
     '''
 
-    authorised_endpoint = authorise_endpoint(SEARCH_BY_INGREDIENTS_ENDPOINT, api_key)
-    ingredients = user_input['Ingredients']
-    authorised_endpoint = add_ingredients_to_endpoint(authorised_endpoint, ingredients)
+    # helper methods to decorate a given endpoint
+    def add_authorisation_endpoint(endpoint, api_key):
+        return endpoint + '?apiKey=' + api_key
+
+    def add_ingredients_to_endpoint(endpoint, ingredient_list):
+        return endpoint + '&ingredients=' + ',+'.join(ingredient_list)
+
+    def add_num_recipes_to_endpoint(endpoint, num_recipes):
+        return endpoint + '&number=' + str(num_recipes)
+
+    def add_query_offset_to_endpoint(endpoint, query_offset):
+        return endpoint + '&offset=' + str(query_offset)
+
+    # extract ingredients key-value from front-end
+    ingredients = query_info.get('Ingredients', None)
+    query_offset = query_info.get('QueryOffset', None)
+    num_recipes_to_query = query_info.get('RecipesToQuery', None)
+
+    # build endpoint url
+    endpoint = add_authorisation_endpoint(original_endpoint, api_key)
+    if ingredients: endpoint = add_ingredients_to_endpoint(endpoint, ingredients)
+    if query_offset: endpoint = add_query_offset_to_endpoint(endpoint, query_offset)
+    if num_recipes_to_query: endpoint = add_num_recipes_to_endpoint(endpoint, num_recipes_to_query)
     
-    # send the query and retrieve the response
-    returned_response = requests.get(authorised_endpoint)
+    return endpoint
+
+def build_list_identifier(lst):
+    return '<{}>'.format('-'.join(lst))
+
+
+def get_recipes_from_ingredients_query(api_key, query_info, aws_information):
+    '''
+    query_info = json file from frontend
+    '''
+
+    # extract ingredients
+    ingredients = query_info['Ingredients']
+    num_ingredients = len(ingredients)
+
+    # TODO: further optimisations
+    # ingredients_sorted = sorted(ingredients)
+    # ingredients_str = build_list_identifier(ingredients_sorted)
+
+    # generate endpoint
+    endpoint = build_endpoint(api_key, query_info, SEARCH_BY_INGREDIENTS_ENDPOINT)
+
+    # send the query to spoonacular and retrieve the response
+    # returned_response = requests.get(endpoint)
+    # returned_json = json.loads(returned_response.text)
+    # dump_json_to_filepath(returned_json, 'sample/findByIngredients.json')
+
+    # TODO: change to get real response
+    returned_json = read_json_from_file('sample/findByIngredients.json')
 
     # parse response
-    returned_json = json.loads(returned_response.text)
     for recipe in returned_json:
-        print('recipe title = {}'.format(recipe['title']))
-        print('used ingredients = {}/{}'
-            .format(recipe['usedIngredientCount'], len(ingredients)))
+        # print(recipe)
+        print("used={}, unused={}, missed={}, num_ingredients={}".format(len(recipe['usedIngredients']), len(recipe['unusedIngredients']), len(recipe['missedIngredients']), num_ingredients))
 
-        unused_ingredients = [i['name'] for i in recipe['missedIngredients']]
-        print('unused ingredients = {}, {}'.format(recipe['missedIngredientCount'], unused_ingredients))
+        # break
+        # print('recipe title = {}'.format(recipe['title']))
+        # print('used ingredients = {}/{}'
+        #     .format(recipe['usedIngredientCount'], len(ingredients)))
+
+        # unused_ingredients = [i['name'] for i in recipe['missedIngredients']]
+        # print('unused ingredients = {}, {}'.format(recipe['missedIngredientCount'], unused_ingredients))
         
+        print("usedIngredients =", [r['name'] for r in recipe['usedIngredients']])
+        print("unusedIngredients =", [r['name'] for r in recipe['unusedIngredients']])
 
-    return authorised_endpoint
+        break
+    
+    for k in returned_json[0]: print(k, end=',')
+
+    return endpoint
 
 
+
+# # # # # # # # # # # #
+#     DECLARATIONS    #
+# # # # # # # # # # # #
 
 SEARCH_BY_INGREDIENTS_ENDPOINT = 'https://api.spoonacular.com/recipes/findByIngredients'
 GET_INGREDIENT_BY_ID_ENDPOINT = ''
 
+MAX_RECIPES_TO_QUERY = 2
+
+# # # # # # # # # # # #
+#  START OF PROGRAM   #
+# # # # # # # # # # # #
+
+# read jsons
 aws_information = read_json_from_file('aws_config.json')
 user_input = read_json_from_file('test_input.json')
 api_key = read_json_from_file('api_config.json')['ApiKey']
 
-print(get_recipes_query(api_key, user_input, aws_information))
+
+resp = get_recipes_from_ingredients_query(api_key, user_input, aws_information)
+print(resp)
