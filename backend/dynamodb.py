8f4780280d98aecb2d8263c1a365eca478108c13
Victor Zhang <vicc.zhang@gmail.com>
2020-11-13 01:06:02 +1100

    removed unnecessary file

---
+++ backend/dynamodb.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+import boto3
+import aws_config as keys
+
+
+dynamodb = boto3.resource('dynamodb',
+                    aws_access_key_id=keys.ACCESS_KEY_ID,
+                    aws_secret_access_key=keys.ACCESS_SECRET_KEY,
+                    region_name=keys.REGION_NAME
+                    )
+
+#dynamodb = boto3.resource('dynamodb')
+
+# Create the DynamoDB table.
+table = dynamodb.create_table(
+    TableName='users',
+    KeySchema=[
+        {
+            'AttributeName': 'email',
+            'KeyType': 'HASH'
+        }
+
+    ],
+    AttributeDefinitions=[
+             {
+            'AttributeName': 'email',
+            'AttributeType': 'S'
+        }
+    ],
+    ProvisionedThroughput={
+        'ReadCapacityUnits': 5,
+        'WriteCapacityUnits': 5
+    }
+)
+
+# Wait until the table exists.
+table.meta.client.get_waiter('table_exists').wait(TableName='users')
+
+# Print out some data about the table.
+print(table.item_count)
